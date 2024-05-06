28533eeed45c860281d72b69b883db9ef2c21346
Fernando Yañez <yanezfer31@gmail.com>
2024-05-06 10:47:29 -0300

    ajuste index.ts para redirecionar

--- app/pages/index.ts
+++ app/pages/index.ts
@@ -4,4 +4,20 @@ import Technologie from "./Technologie";
 import Projects from "./Projects";
 import Contact from "./Contact";
 
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 export { HeroSection, About, Technologie, Projects, Contact };
+
+
+const IndexPage = () => {
+    const router = useRouter();
+  
+    useEffect(() => {
+      router.push('/app/page');
+    }, []);
+  
+    return null;
+  };
+
+  export default IndexPage;
